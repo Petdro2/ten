a727602436b128d98dfecb8733ae3d6ffd57581f
Petdro2 <petdrobyka@gmail.com>
2024-10-31 14:09:25 +0200

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,401 @@
+const products = [
+    {   
+        id: "beercard1",
+        name: "BROWN PORTHER:HAZELNUT EDITION",
+        price: [
+            "110 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer4.png"
+        ],
+        sore:[
+            "Sore:Brown Porter",
+        ],
+        alcohol:[
+            "ALC. 5.7%"
+        ],
+        og:[
+            " OG: 16.5%"
+        ],                      
+        ibu:[
+            "IBU:25"
+        ]
+    },
+    {   
+        id: "beercard2",
+        name: "ten men ipa",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer5.png"
+        ],
+        sore:[
+            "Sore:IPA WEST COAST",
+        ],
+        alcohol:[
+            "ALC. 6%"
+        ],
+        og:[
+            " OG: 15%"
+        ],                      
+        ibu:[
+            "IBU:50"
+        ]
+    },
+    {   
+        id: "beercard3",
+        name: " NOT FOR BREAKFAST: black currant cheesecake",
+        price: [
+            "124 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer6.png"
+        ],
+        sore: [
+            "Sore:Pastry Sour Fruited",
+        ],
+        alcohol:[
+            "ALC:4.6%"
+        ],
+        og:[
+            "OG:16%"
+        ],
+        ibu:[
+            "IBU:16"
+        ]
+    },
+    {   
+        id: "beercard4",
+        name: "DRIVE&DIVE",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer7.png"
+        ],
+        sore:[
+            "IPA East Coast",
+        ],
+        alcohol:[
+            "ALC. 5.2%"
+        ],
+        og:[
+            " OG: 5%"
+        ],                      
+        ibu:[
+            "IBU:0"
+        ]
+    },
+    {   
+        id: "beercard5",
+        name: "MOLOKO STOUT",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer8.png"
+        ],
+        sore:[
+            "Milk Stout",
+        ],
+        alcohol:[
+            "ALC. 5.2%"
+        ],
+        og:[
+            " OG: 18%"
+        ],                      
+        ibu:[
+            "IBU:18"
+        ]
+    },
+    {   
+        id: "beercard2",
+        name: "ten men ipa",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer5.png"
+        ],
+        sore:[
+            "Sore:IPA WEST COAST",
+        ],
+        alcohol:[
+            "ALC. 6%"
+        ],
+        og:[
+            " OG: 15%"
+        ],                      
+        ibu:[
+            "IBU:50"
+        ]
+    },
+    {   
+        id: "beercard1",
+        name: "BROWN PORTHER:HAZELNUT EDITION",
+        price: [
+            "110 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer4.png"
+        ],
+        sore:[
+            "Sore:Brown Porter",
+        ],
+        alcohol:[
+            "ALC. 5.7%"
+        ],
+        og:[
+            " OG: 16.5%"
+        ],                      
+        ibu:[
+            "IBU:25"
+        ]
+    },
+    {   
+        id: "beercard2",
+        name: "ten men ipa",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer5.png"
+        ],
+        sore:[
+            "Sore:IPA WEST COAST",
+        ],
+        alcohol:[
+            "ALC. 6%"
+        ],
+        og:[
+            " OG: 15%"
+        ],                      
+        ibu:[
+            "IBU:50"
+        ]
+    },
+    {   
+        id: "beercard3",
+        name: " NOT FOR BREAKFAST: black currant cheesecake",
+        price: [
+            "124 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer6.png"
+        ],
+        sore: [
+            "Sore:Pastry Sour Fruited",
+        ],
+        alcohol:[
+            "ALC:4.6%"
+        ],
+        og:[
+            "OG:16%"
+        ],
+        ibu:[
+            "IBU:16"
+        ]
+    },
+    {   
+        id: "beercard4",
+        name: "DRIVE&DIVE",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer7.png"
+        ],
+        sore:[
+            "IPA East Coast",
+        ],
+        alcohol:[
+            "ALC. 5.2%"
+        ],
+        og:[
+            " OG: 5%"
+        ],                      
+        ibu:[
+            "IBU:0"
+        ]
+    },
+    {   
+        id: "beercard5",
+        name: "MOLOKO STOUT",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer8.png"
+        ],
+        sore:[
+            "Milk Stout",
+        ],
+        alcohol:[
+            "ALC. 5.2%"
+        ],
+        og:[
+            " OG: 18%"
+        ],                      
+        ibu:[
+            "IBU:18"
+        ]
+    },
+    {   
+        id: "beercard2",
+        name: "ten men ipa",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer5.png"
+        ],
+        sore:[
+            "Sore:IPA WEST COAST",
+        ],
+        alcohol:[
+            "ALC. 6%"
+        ],
+        og:[
+            " OG: 15%"
+        ],                      
+        ibu:[
+            "IBU:50"
+        ]
+    },
+    {   
+        id: "beercard1",
+        name: "BROWN PORTHER:HAZELNUT EDITION",
+        price: [
+            "110 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer4.png"
+        ],
+        sore:[
+            "Sore:Brown Porter",
+        ],
+        alcohol:[
+            "ALC. 5.7%"
+        ],
+        og:[
+            " OG: 16.5%"
+        ],                      
+        ibu:[
+            "IBU:25"
+        ]
+    },
+    {   
+        id: "beercard2",
+        name: "ten men ipa",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer5.png"
+        ],
+        sore:[
+            "Sore:IPA WEST COAST",
+        ],
+        alcohol:[
+            "ALC. 6%"
+        ],
+        og:[
+            " OG: 15%"
+        ],                      
+        ibu:[
+            "IBU:50"
+        ]
+    },
+    {   
+        id: "beercard3",
+        name: " NOT FOR BREAKFAST: black currant cheesecake",
+        price: [
+            "124 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer6.png"
+        ],
+        sore: [
+            "Sore:Pastry Sour Fruited",
+        ],
+        alcohol:[
+            "ALC:4.6%"
+        ],
+        og:[
+            "OG:16%"
+        ],
+        ibu:[
+            "IBU:16"
+        ]
+    },
+    {   
+        id: "beercard4",
+        name: "DRIVE&DIVE",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer7.png"
+        ],
+        sore:[
+            "IPA East Coast",
+        ],
+        alcohol:[
+            "ALC. 5.2%"
+        ],
+        og:[
+            " OG: 5%"
+        ],                      
+        ibu:[
+            "IBU:0"
+        ]
+    },
+    {   
+        id: "beercard5",
+        name: "MOLOKO STOUT",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer8.png"
+        ],
+        sore:[
+            "Milk Stout",
+        ],
+        alcohol:[
+            "ALC. 5.2%"
+        ],
+        og:[
+            " OG: 18%"
+        ],                      
+        ibu:[
+            "IBU:18"
+        ]
+    },
+    {   
+        id: "beercard2",
+        name: "ten men ipa",
+        price: [
+            "109 uan"
+        ],
+        photo: [
+            "../img/beer-img/beer5.png"
+        ],
+        sore:[
+            "Sore:IPA WEST COAST",
+        ],
+        alcohol:[
+            "ALC. 6%"
+        ],
+        og:[
+            " OG: 15%"
+        ],                      
+        ibu:[
+            "IBU:50"
+        ]
+    },
+];
+
+
+export default products;
